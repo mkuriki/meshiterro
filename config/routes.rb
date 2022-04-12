ff7d61da840610a4f6c2f7755580eddce5333b0f
mkuriki <101915061+mkuriki@users.noreply.github.com>
2022-04-12 09:34:58 +0000

    [Finish]4-14 【ユーザーに関する機能(1) - 準備編

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-resources :post_images, only: [:new, :index, :show]
-  devise_for :users
+  
+  
+  resources :users, only: [:show, :edit]
   get 'homes/top'
   get 'homes/about', as: 'about'
   
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
