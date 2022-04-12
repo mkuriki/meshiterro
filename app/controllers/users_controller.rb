d14344306c63e48b07a57cd3d2f65b8446442274
mkuriki <101915061+mkuriki@users.noreply.github.com>
2022-04-12 11:36:16 +0000

    [Finish]【ユーザーに関する機能(3) - ユーザー情報の編集】

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
   end
+
+  
 end
